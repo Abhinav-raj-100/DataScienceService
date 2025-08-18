3e71c85c0ca5f8441ebbf563077a24e25aec2bbe
Abhinav-raj-100 <awesomeboy1304@gmail.com>
2025-08-18 22:56:43 +0530

    small change

--- src/app/utils/messageUtil.py
+++ src/app/utils/messageUtil.py
@@ -2,7 +2,7 @@ import re
 
 class MessageUtil:
     def isBankSms(self,message):
-        words_to_search = ["bank","account","transaction","withdrawal","spent","card","bank"]
+        words_to_search = ["bank","account","transaction","withdrawal","spent","card",]
         pattern = r'\b(?:'+'|'.join(re.escape(word) for word in words_to_search)+r')\b'
         return bool(re.search(pattern,message,flags=re.IGNORECASE))
 
